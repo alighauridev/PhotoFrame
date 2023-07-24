59f09095a1a41dd15a7a6cc69969048921f97a7e
tachyontechs <hafizabdullahqurashi1@gmail.com>
2023-07-25 00:24:15 +0500

    update

--- client/src/Pages/Favi.js
+++ client/src/Pages/Favi.js
@@ -6,7 +6,7 @@ const Favi = () => {
      <div style={{background:'#F8F8F8',padding:'80px 0'}}>
       <div>
         <center><h1 style={{fontFamily:'Montserrat, system-ui',fontSize:'48px',fontWeight:'600',margin:'0',paddingBottom:'40px'}}>Top Trending and Favorite Frames</h1></center>
-        <center><p style={{fontFamily:'Montserrat, system-ui',fontSize:'18px',fontWeight:'400',margin:'0',paddingBottom:'40px'}}>Painted canvases are unique gifts that convey deep emotions and memories. Add a personal touch to your special occasion <br /> and surprise your loved ones with a work of art that comes from the heart.</p></center>
+        <center><p style={{fontFamily:'Montserrat, system-ui',fontSize:'18px',fontWeight:'400',margin:'0',paddingBottom:'40px',lineHeight:'30px'}}>Painted canvases are unique gifts that convey deep emotions and memories. Add a personal touch to your special occasion <br /> and surprise your loved ones with a work of art that comes from the heart.</p></center>
       </div>
    <div style={{display:'flex',justifyContent:"center",gap:'40px',marginTop:'40px'}}>
     <div style={{position:'relative'}} >
--- client/src/Pages/Unique.js
+++ client/src/Pages/Unique.js
@@ -6,7 +6,7 @@ const Unique = () => {
     <div style={{padding:'80px 0'}}>
      <div>
        <center><h1 style={{fontFamily:'Montserrat, system-ui',fontSize:'48px',fontWeight:'600',margin:'0',paddingBottom:'40px'}}>unique gift</h1></center>
-       <center><p style={{fontFamily:'Montserrat, system-ui',fontSize:'18px',fontWeight:'400',margin:'0',paddingBottom:'40px',color:'#00000080'}}>Painted canvases are unique gifts that convey deep emotions and memories. Add a personal touch to your special occasion <br />
+       <center><p style={{fontFamily:'Montserrat, system-ui',fontSize:'18px',fontWeight:'400',margin:'0',paddingBottom:'40px',color:'#00000080',lineHeight:'30px'}}>Painted canvases are unique gifts that convey deep emotions and memories. Add a personal touch to your special occasion <br />
         and surprise your loved ones with a work of art that comes from the heart.</p></center>
      </div>
      <div style={{display:'flex',justifyContent:'center',gap:'40px'}}>
